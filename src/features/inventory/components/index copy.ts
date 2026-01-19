fddd22b34b1745038edaa4ac7f07a05228db6d6f
WebVishnu <vishnugoswami6000@gmail.com>
2026-01-19 18:41:45 +0530

    batch tracking added

---
+++ src/features/inventory/components/index copy.ts
@@ -0,0 +1,8 @@
+export { ItemMaster } from './ItemMaster';
+export { LocationManagement } from './LocationManagement';
+export { MovementManagement } from './MovementManagement';
+export { BatchManagement } from './BatchManagement';
+export { ExpiryMonitoring } from './ExpiryMonitoring';
+export { StockCounting } from './StockCounting';
+export { InventoryReports } from './InventoryReports';
+export { ProductDetailPage } from './ProductDetailPage';
